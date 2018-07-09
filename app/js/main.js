7414740e941f191fa873c13b085613ca0467f355
Vitaliy Nickishin <nickishin34@gmail.com>
2018-07-10 00:55:45 +0300

    5 section is ready

---
+++ app/js/main.js
@@ -0,0 +1,46 @@
+
+$(".dial_1").knob(
+{
+ 'readOnly': true, //отмена прокрутки
+ 'thickness': 0.05, //толщина индикатора 5%
+ 'fgColor': '#10b292', //цвет заполняющей полоски $active-color
+ 'bgColor': '#047378', //цвет фона
+ 'angleOffset': '-285', //направление полосы
+ 'width': '122', //размеры круга
+ 'height': '122',
+ 'fontWeight': '400', //вес  шрифта
+ 'inputColor': '#a3a6ac', //цвет шрифта
+ 'font': 'Open Sans'
+});
+
+$(".dial_2").knob(
+{
+ 'readOnly': true, 
+ 'thickness': 0.05, 
+ 'fgColor': '#10b292', 
+ 'bgColor': '#047378',
+ 'angleOffset': '-270',
+ 'width': '122',
+ 'height': '122',
+ 'fontWeight': '400', 
+ 'inputColor': '#a3a6ac',
+
+});
+
+$(".dial_3").knob(
+{
+ 'readOnly': true, 
+ 'thickness': 0.05, 
+ 'fgColor': '#10b292',
+ 'bgColor': '#047378',
+ 'angleOffset': '-215',
+ 'width': '122',
+ 'height': '122',
+ 'fontWeight': '400', 
+ 'inputColor': '#a3a6ac',
+});
+
+
+ 
+
+
